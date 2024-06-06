c2835f8972fb055250b219557d95550d8b238dbb
Daniel Zoba <daniel.sobe@sorben.com>
2024-06-06 14:49:49 +0200

    basic dockerization

---
+++ docker/startme.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pushd uploader-recny-model
+echo "REACT_APP_API_URL_TRANSCRIPT=$BACKEND_URL_RECOG" > .env
+echo "REACT_APP_API_URL_SLOWNIK=$BACKEND_URL_DICT" >> .env
+popd
+
+cd application && npm start
