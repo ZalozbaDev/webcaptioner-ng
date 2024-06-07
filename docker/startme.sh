3cd2fc4efc8d588abce7f4ff44805190ad55da3a
Daniel Zoba <daniel.sobe@sorben.com>
2024-06-07 12:35:26 +0000

    extend docker config appropriately

--- docker/startme.sh
+++ docker/startme.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 pushd application
-echo "REACT_APP_VOSK_SERVER_URL=$REACT_APP_VOSK_SERVER_URL" > .env
+echo "REACT_APP_VOSK_SERVER_URL=$REACT_APP_VOSK_SERVER_URL"   > .env
+echo "REACT_APP_SOTRA_SERVER_URL=$REACT_APP_SOTRA_SERVER_URL" > .env
+echo "REACT_APP_YOUTUBE_REGION=$REACT_APP_YOUTUBE_REGION"     > .env
+
 popd
 
 cd application && npm start
