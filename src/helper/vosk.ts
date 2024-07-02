b9eca59c7d6808aa0e1403d326938713aae084b1
FS <f.scholze@gmx.de>
2024-07-02 10:52:14 +0200

    Handle milliseconds to vosk rsponse

--- src/helper/vosk.ts
+++ src/helper/vosk.ts
@@ -1,11 +1,23 @@
 export const typedVoskResponse = (data: any): VOSKResponse => {
   const typed = JSON.parse(data)
   typed.listen = typed.listen === 'true'
-  typed.start = typed.start ? parseInt(typed.start) : undefined
-  typed.stop = typed.stop ? parseInt(typed.stop) : undefined
+  const startMs = typed.startMs ? ('000' + typed.startMs).slice(-3) : '000'
+  const stopMs = typed.stopMs ? ('000' + typed.stopMs).slice(-3) : '000'
+  typed.start = typed.start ? parseInt(`${typed.start}${startMs}`) : undefined
+  typed.stop = typed.stop ? parseInt(`${typed.stop}${stopMs}`) : undefined
+
+  console.log({
+    text: typed.text,
+    start: `${typed.start}${startMs}`,
+    stop: `${typed.stop}${stopMs}`,
+  })
 
   return typed
 }
+
+const MAX_LENGTH_SECONDS = 5
+const MAX_LENGTH_WORDS = 4
+const MAX_LENGTH_CHARS = 40
 
 export const createYoutubePackages = (
   text: string,
@@ -15,7 +27,16 @@ export const createYoutubePackages = (
   const textLength = text.length
   const textWordsLength = text.split(' ').length
 
-  if (duration <= 5) {
+  // if (textLength <= MAX_LENGTH_CHARS) {
+  //   if (textWordsLength <= MAX_LENGTH_WORDS) {
+  //     if (duration <= MAX_LENGTH_SECONDS) {
+  //       return [{ date: timestamps.start, text: text }]
+  //     } else {
+  //     }
+  //   }
+  // }
+
+  if (duration <= MAX_LENGTH_SECONDS) {
     return [{ date: timestamps.start, text: text }]
   }
 
