92f75e8f485cca9cee1bfe12eb8c6ed57ddc8b97
Daniel Zoba <daniel.sobe@sorben.com>
2024-08-29 15:59:50 +0200

    import envvars into Docker container

--- docker/startme.sh
+++ docker/startme.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 pushd application
-echo "REACT_APP_WEBCAPTIONER_SERVER=$REACT_APP_WEBCAPTIONER_SERVER" >  .env
-echo "REACT_APP_YOUTUBE_REGION=$REACT_APP_YOUTUBE_REGION"           >> .env
-echo "REACT_APP_PASSWORD=$REACT_APP_PASSWORD"                       >> .env
+echo "REACT_APP_WEBCAPTIONER_SERVER=$REACT_APP_WEBCAPTIONER_SERVER"                 >  .env
+echo "REACT_APP_YOUTUBE_REGION=$REACT_APP_YOUTUBE_REGION"                           >> .env
+echo "REACT_APP_PASSWORD=$REACT_APP_PASSWORD"                                       >> .env
+echo "REACT_APP_DEFAULT_SOTRA_MODEL=$REACT_APP_DEFAULT_SOTRA_MODEL"                 >> .env
+echo "REACT_APP_DEFAULT_YOUTUBE_TIME_OFFSET=$REACT_APP_DEFAULT_YOUTUBE_TIME_OFFSET" >> .env
 
 popd
 
