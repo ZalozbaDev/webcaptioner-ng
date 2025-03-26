f721b68fccd9b7e9a2cb9bfeed2caed792a7a57f
FS <f.scholze@gmx.de>
2025-03-26 08:46:13 +0100

    Make settings box scrollable

--- src/features/main-screen/components/record-buttons-container/settings-container.tsx
+++ src/features/main-screen/components/record-buttons-container/settings-container.tsx
@@ -191,31 +191,35 @@ export const SettingsContainer: FC<{
     <Menu
       onClose={onClose}
       anchorEl={anchorEl}
+      disableScrollLock
       open={open}
-      PaperProps={{
-        elevation: 0,
-        sx: {
-          border: '1px solid black',
-          overflow: 'visible',
-          filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-          mt: 0.5,
-          '& .MuiAvatar-root': {
-            width: 32,
-            height: 32,
-            ml: -0.5,
-            mr: 1,
-          },
-          '&::before': {
-            content: '""',
-            display: 'block',
-            position: 'absolute',
-            top: 0,
-            right: 14,
-            width: 10,
-            height: 10,
-            bgcolor: 'background.paper',
-            transform: 'translateY(-50%) rotate(45deg)',
-            zIndex: 0,
+      slotProps={{
+        paper: {
+          elevation: 0,
+          sx: {
+            border: '1px solid black',
+            maxHeight: '90%',
+            // overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 0.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            // '&::before': {
+            //   content: '""',
+            //   display: 'block',
+            //   position: 'absolute',
+            //   top: 0,
+            //   right: 14,
+            //   width: 10,
+            //   height: 10,
+            //   bgcolor: 'background.paper',
+            //   transform: 'translateY(-50%) rotate(45deg)',
+            //   zIndex: 0,
+            // },
           },
         },
       }}
